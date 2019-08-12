6f5be8fa435ed835b3ae45200c3284dea5d00afe
Alec Hall <alecnhall@gmail.com>
2019-08-12 23:42:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  badge_messages = []
   array.each do |name|
-    return badge_messages.push("Hello, my name is #{name}.")
+    return array.push("Hello, my name is #{name}.")
   end
   return badge_messages
+end
+
+def assign_rooms
+  array.each_with_index do |name, index|
 end
