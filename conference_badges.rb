959162e583f5a16a21bbe1a08989dac9dad7865f
Alec Hall <alecnhall@gmail.com>
2019-08-13 00:52:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array[0])
-  puts assign_rooms(array[0])
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
